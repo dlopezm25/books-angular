44c04778e9ddd1e04ecb3f66372284b9977ee66a
elcrapula <elcrapula_8@hotmail.com>
2020-03-05 14:16:45 -0500

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SigninComponent } from './login/signin/signin.component';
-import { SignoutComponent } from './login/signout/signout.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SigninComponent } from "./login/signin/signin.component";
+import { SignupComponent } from './login/signup/signup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SigninComponent,
-    SignoutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SigninComponent, SignupComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
